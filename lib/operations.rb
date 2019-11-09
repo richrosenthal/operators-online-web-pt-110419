3d24d1cf6b4992b97330812378fda101e53ae41f
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-09 23:54:23 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,6 +2,12 @@ def unsafe?(speed)
  speed = 61
  answer = false
   speed > 60 ? answer = true : answer = false
+  speed < 40 ? answer = true : answer = false
+  if speed >= 40 && speed <= 60
+    answer = false
+  else 
+    answer = true
+  end  
   answer
 end
 
