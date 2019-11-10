997ad354776bef53c68e15c85933b24ceb3d0792
Richard Rosenthal <richard.rosenthal@acd.ccac.edu>
2019-11-10 00:03:03 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,23 @@
 def unsafe?(speed)
-
+ speed
+ answer = false
+  speed > 60 ? answer = true : answer = false
+  speed < 40 ? answer = true : answer = false
+  if speed >= 40 && speed <= 60
+    answer = false
+  else 
+    answer = true 
+  end  
+  answer
 end
 
 
 
 def not_safe?(speed)
+  answer = true
+	speed < 40 || speed > 60 ? answer = true : answer = false
+	answer
+	  
 	
 end
 	
